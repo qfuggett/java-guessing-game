d8a63b558c60834cddfe3eb06434dc80410923ef
qfuggett <qfuggett@gmail.com>
2021-06-01 15:54:29 +0000

    added user input for number and created a random number

--- Game.java
+++ Game.java
@@ -4,17 +4,26 @@ A number-guessing game.
 
 */
 import java.util.Scanner;
+import java.util.Random;
 
 public class Game {
   public static void main(String[] args){
     System.out.println("Hello there! May I have your name?");
 
+    //Greet the player
     Scanner input = new Scanner(System.in);
     String name = input.nextLine();
-    System.out.println("Hey," + name + "I'm thinking of a number between 1 and 100.");
-    System.out.println("Try to guess my number");
+    System.out.println("Hey, " + name + " I'm thinking of a number between 1 and 100.");
+    System.out.println("Try to guess my number!");
 
-    
+    //This is our number that the user is trying to guess
+    Random rand = new Random();
+    int number = rand.nextInt(100);
+
+    //User inputs number
+    int user_input = input.nextInt();
+    System.out.println("Here is your number: " + user_input + " Here is the random number: " + number);
+
 
     input.close();
 
