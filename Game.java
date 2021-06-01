2aa1475f3bc8e0ea75c81b1b529fc86b10472bc9
qfuggett <qfuggett@gmail.com>
2021-06-01 15:42:32 +0000

    player greeting

--- Game.java
+++ Game.java
@@ -3,10 +3,21 @@
 A number-guessing game.
 
 */
+import java.util.Scanner;
 
 public class Game {
   public static void main(String[] args){
-    System.out.println("Hello there!");
+    System.out.println("Hello there! May I have your name?");
+
+    Scanner input = new Scanner(System.in);
+    String name = input.nextLine();
+    System.out.println("Hey," + name + "I'm thinking of a number between 1 and 100.");
+    System.out.println("Try to guess my number");
+
+    
+
+    input.close();
+
   }
 
 }
